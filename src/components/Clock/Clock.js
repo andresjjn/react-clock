394a2501a9397ac4d69aea7ffdfa62b56de64066
Andres Jejen <andresjt93@gmail.com>
2021-11-03 19:26:54 -0500

    Add build app update

--- src/components/Clock/Clock.js
+++
@@ -1,6 +0,0 @@
-export default function Clock({ time }) {
-
-  return (
-    <h1 className="clock"> {time} </h1>
-  )
-}
--- src/utils/getTime.js
+++
@@ -1,8 +0,0 @@
-export default function getTime() {
-  const time = new Date();
-  const meridian = time.getHours() >= 12 ? 'PM' : 'AM';
-  const hours = time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
-  const minutes = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
-  const seconds = time.getSeconds() < 10 ? `0${time.getSeconds()}` : time.getSeconds();
-  return `${hours === 0 ? 12 : hours}:${minutes}:${seconds} ${meridian}`;
-};
