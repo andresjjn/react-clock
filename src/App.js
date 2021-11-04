504fb5d4a4308c997bf9283a95c2285ddacc9036
Andres Jejen <andresjt93@gmail.com>
2021-11-03 19:03:25 -0500

    Created the funtionality and UI is working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Clock from './components/Clock/Clock';
+import getTime from './utils/getTime';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [showTime, setShowTime] = useState('Starting...')
+
+  useEffect(() => {
+    const interval = setInterval(() => 
+      setShowTime(getTime())
+    , 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Clock time={showTime}/>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,6 @@
+export default function Clock({ time }) {
+
+  return (
+    <h1 className="clock"> {time} </h1>
+  )
+}
